e7df2bdef5eb4e45eb9fb804646e492ee0ca61c1
Filip Żegleń <filipmikolajgabriel@gmail.com>
2021-04-07 13:45:28 +0200

    Refactor object name from UserRole to Role

--- src/main/java/pl/filipzeglen/springsecuritywithdb/model/repository/UserRoleRepository.java
+++ src/main/java/pl/filipzeglen/springsecuritywithdb/model/repository/UserRoleRepository.java
@@ -2,8 +2,8 @@ package pl.filipzeglen.springsecuritywithdb.model.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pl.filipzeglen.springsecuritywithdb.model.account.UserRole;
+import pl.filipzeglen.springsecuritywithdb.model.account.Role;
 
 @Repository
-public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
+public interface UserRoleRepository extends CrudRepository<Role, Long> {
 }
